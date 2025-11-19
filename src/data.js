bccf2f461131961539b18eb3abd0bc88322cd2ad
Thisen Nambukara <thisennambukara@gmail.com>
2025-11-19 17:41:41 +0530

    add new certificates

--- src/data.js
+++ src/data.js
@@ -15,7 +15,8 @@ export default Image;
 import Proyek1 from "/assets/proyek/proyek1.png";
 import Proyek2 from "/assets/proyek/proyek2.jpeg";
 import Proyek3 from "/assets/proyek/proyek3.png";
-import Proyek4 from "/assets/proyek/proyek2.jpeg";
+import Proyek4 from "/assets/proyek/1751027808122.jpg";
+import Proyek5 from "/assets/proyek/1751028177653.jpg";
 
 
 export const listProyek = [
@@ -48,9 +49,18 @@ export const listProyek = [
   {
     id: 4,
     gambar: Proyek4,
-    nama: "Ceylon Safari! 🐘🚙",
-    desk: "A full-stack web platform developed to streamline wildlife safari tour management in Sri Lanka.",
-    tools: ["HTML", "Javascript", "Java", "TailwindCSS", "sql server"],
+    nama: "PulseMind 🫀🧠",
+    desk: " Automated Hhealthcare monitoring system through a wearable device on a wrist which focused on measuring both mental and physical health condition of patients.",
+    tools: ["C++","HTML", "Javascript",  "CSS", "My sql"],
+    dad: "400",
+    sourcecode: "https://github.com/nambukara/Ceylon_Safari",
+  },
+  {
+    id: 5,
+    gambar: Proyek5,
+    nama: "LIFORA 🩸🫂",
+    desk: "Java-based Blood type compatibility checking system developed to support life-saving decisions in critical medical scenarios.",
+    tools: ["Java"],
     dad: "400",
     sourcecode: "https://github.com/nambukara/Ceylon_Safari",
   },
@@ -64,39 +74,57 @@ import cert1 from '/assets/Certifications/UC-4b536295-cc2b-4dcc-9f69-ba059bce751
 import cert2 from '/assets/Certifications/Front-End_Web_Development_E-Certificate_page-0001.jpg';
 import cert3 from '/assets/Certifications/Web_Design_for_Beginners_E-Certificate-1.png';
 import cert4 from '/assets/Certifications/Python_for_Beginners_E-Certificate_page-0001.jpg';
+import cert5 from '/assets/Certifications/1751731062003.jpg';
+import cert6 from '/assets/Certifications/OOP Java-1.png';
 
 export const certificates = [
   {
     id: 1,
-    title: 'React - The Complete Guide',
-    issuer: 'Udemy | Maximilian Schwarzmüller',
-    date: 'March 2024',
+    title: 'Learn to Code HTML & CSS for Responsive Real-World Websites ',
+    issuer: 'Udemy | Metla Sudha Sekhar',
+    date: 'Jul 2024',
     image: cert1,
-    credentialUrl: 'https://ude.my/UC-abc123',
+    credentialUrl: 'https://www.udemy.com/certificate/UC-4b536295-cc2b-4dcc-9f69-ba059bce751a/',
   },
   {
     id: 2,
-    title: 'Node.js - The Complete Guide',
-    issuer: 'Udemy',
-    date: 'June 2024',
+    title: 'Front-End Web Development',
+    issuer: 'University of Moratuwa',
+    date: 'Dec 2024',
     image: cert2,
-    credentialUrl: 'https://ude.my/UC-xyz789',
+    credentialUrl: 'https://www.linkedin.com/in/thisen-nambukara-02464323b/details/certifications/1750476700719/single-media-viewer/?profileId=ACoAADu3kFwBYNUXmPOsFTthA9RMQScqdVPVdI8',
   },
   {
     id: 3,
-    title: 'Node.js - The Complete Guide',
-    issuer: 'Udemy',
-    date: 'June 2024',
+    title: 'Web Design for Beginners',
+    issuer: 'University of Moratuwa',
+    date: 'Mar 2023',
     image: cert3,
-    credentialUrl: 'https://ude.my/UC-xyz789',
+    credentialUrl: 'https://www.linkedin.com/in/thisen-nambukara-02464323b/details/certifications/1750834817400/single-media-viewer/?profileId=ACoAADu3kFwBYNUXmPOsFTthA9RMQScqdVPVdI8',
   },
   {
     id: 4,
-    title: 'Node.js - The Complete Guide',
-    issuer: 'Udemy',
-    date: 'June 2024',
-    image: cert3,
-    credentialUrl: 'https://ude.my/UC-xyz789',
+    title: 'Python for Beginners',
+    issuer: 'University of Moratuwa',
+    date: 'Nov 2024',
+    image: cert4,
+    credentialUrl: 'https://www.linkedin.com/in/thisen-nambukara-02464323b/details/certifications/1750990121752/single-media-viewer/?profileId=ACoAADu3kFwBYNUXmPOsFTthA9RMQScqdVPVdI8',
+  },
+  {
+    id: 5,
+    title: 'AI/ML Engineer - Stage 1',
+    issuer: 'SLIIT',
+    date: 'Jul 2025',
+    image: cert5,
+    credentialUrl: 'https://code.sliit.org/certificates/eyxgvfjypm',
+  },
+  {
+    id: 6,
+    title: 'OOPs in Java',
+    issuer: 'Simplilearn',
+    date: 'Jan 2025',
+    image: cert6,
+    credentialUrl: 'https://www.simplilearn.com/skillup-certificate-landing?token=eyJjb3Vyc2VfaWQiOiI0Mjc2IiwiY2VydGlmaWNhdGVfdXJsIjoiaHR0cHM6XC9cL2NlcnRpZmljYXRlcy5zaW1wbGljZG4ubmV0XC9zaGFyZVwvNzc4NTg3MF83MDE0NjM0MTczNzE2ODcyMTI0OC5wbmciLCJ1c2VybmFtZSI6IlRoaXNlbiBOYW1idWthcmEifQ%3D%3D&utm_source=shared-certificate&utm_medium=lms&utm_campaign=shared-certificate-promotion&referrer=https%3A%2F%2Flms.simplilearn.com%2Fdashboard%2Fcertificate&%24web_only=true&_branch_match_id=1519660710186655930&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXL87MLcjJ1EssKNDLyczL1k%2FVL8p0rgxN83T3C02yrytKTUstKsrMS49PKsovL04tsvUBqkpN8cwDAIthNEBBAAAA',
   },
 ]
 
